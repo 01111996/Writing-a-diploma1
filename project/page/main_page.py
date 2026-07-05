d1b43e1cf416013ab45cf6d84814d75651d24432
Евгения <e.gasykova@gmail.com>
2026-07-05 22:59:45 +0300

    Что снова не так?

--- project/page/main_page.py
+++ project/page/main_page.py
@@ -1,14 +1,16 @@
 from project.page.base_page import BasePage
 from project.page.payment_page import PaymentPage
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import EC
 
 
 class MainPage(BasePage):
     BUY_BUTTON = (By.ID, "button-book") 
 
     def click_buy(self):
-        button = self.wait.until(EC.element_to_be_clickable(self.BUY_BUTTON))
-        button.click()
+    button = self.wait.until(EC.presence_of_element_located(self.BUY_BUTTON))
+    button = self.wait.until(EC.element_to_be_clickable(button))
+    button.click()
 
     def click_credit(self):
         self.find(self.CREDIT_BUTTON).click()
