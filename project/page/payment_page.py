f95b8bff42d851490cbdc7a76f7bb1224ee92282
Евгения <e.gasykova@gmail.com>
2026-07-07 14:24:26 +0300

    ?

--- project/page/payment_page.py
+++ project/page/payment_page.py
@@ -5,12 +5,12 @@ from project.page.base_page import BasePage
 
 class PaymentPage(BasePage):
     CARD_NUMBER = (By.XPATH, "//input[@placeholder='0000 0000 0000 0000']")
-    EXPIRY_MONTH = (By.XPATH, "//input[@placeholder='06']")
-    EXPIRY_YEAR = (By.XPATH, "//input[@placeholder='15']")
-    OWNER_NAME = (By.XPATH, "//label[normalize-space()='Владелец']/following::input[1]")
-    CVC = (By.XPATH, "//input[@placeholder='357']")
-    PAY_BUTTON = (By.XPATH, "//button[contains(normalize-space(),'Продолжить') or contains(normalize-space(),'Отправить')]")
-
+    EXPIRY_MONTH = (By.XPATH, "//label[normalize-space()='Месяц']/following::input[1]")
+    EXPIRY_YEAR  = (By.XPATH, "//label[normalize-space()='Год']/following::input[1]")
+    OWNER_NAME   = (By.XPATH, "//label[normalize-space()='Владелец']/following::input[1]")
+    CVC          = (By.XPATH, "//label[contains(normalize-space(),'CVC')]/following::input[1]")
+    PAY_BUTTON   = (By.XPATH, "//button[contains(normalize-space(),'Продолжить') or contains(normalize-space(),'Отправить')]")
+    
     def fill_card(self, card_data):
         number_field = self.wait.until(EC.presence_of_element_located(self.CARD_NUMBER))
         number_field.clear()
@@ -20,6 +20,6 @@ class PaymentPage(BasePage):
         self.find(self.EXPIRY_YEAR).send_keys(card_data["year"])
         self.find(self.OWNER_NAME).send_keys(card_data["owner"])
         self.find(self.CVC).send_keys(card_data["cvc"])
-    
+        
     def pay(self):
         self.find(self.PAY_BUTTON).click()
