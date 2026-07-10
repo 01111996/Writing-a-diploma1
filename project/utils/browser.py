278853e0ce8e850a5d0c5537dffd816ce36957b9
Евгения <e.gasykova@gmail.com>
2026-07-11 01:17:03 +0300

    ???

--- project/utils/browser.py
+++ project/utils/browser.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 def create_driver():
     options = Options()
-    options.add_argument('--headless=new')
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
